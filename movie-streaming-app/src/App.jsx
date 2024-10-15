a6b0feefac20a4aebbb649d232df3a58d82a410c
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-10-16 01:41:09 +0530

    Undo the changes of App.jsx

--- movie-streaming-app/src/App.jsx
+++ movie-streaming-app/src/App.jsx
@@ -5,17 +5,12 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind CSS is working!
-      </h1>
-    </div>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     {/* <Route path="/movies/:id" element={<MovieDetail />} /> */}
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/movies/:id" element={<MovieDetail />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
