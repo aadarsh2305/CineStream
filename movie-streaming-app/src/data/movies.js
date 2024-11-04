b72d9ab0145a52a17bd591325d3f1ea9e968ff80
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-11-04 12:56:53 +0530

    resolved some basic issue with .vite and images

---
+++ movie-streaming-app/src/data/movies.js
@@ -0,0 +1,26 @@
+import Inception from "../assets/Images/Inception.jpg";
+import Matrix from "../assets/Images/Matrix.webp"
+
+export const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      description: "A mind-bending thriller by Christopher Nolan.",
+      thumbnail: Inception,
+      videoUrl: "/movies/inception.mp4",
+      year: 2010,
+      genre: "Sci-Fi, Thriller",
+      duration: "2h 28m"
+    },
+    {
+      id: 2,
+      title: "The Matrix",
+      description: "A hacker discovers a shocking truth about reality.",
+      thumbnail: Matrix,
+      videoUrl: "/movies/matrix.mp4",
+      year: 1999,
+      genre: "Action, Sci-Fi",
+      duration: "2h 16m"
+    }
+  ];
+  
